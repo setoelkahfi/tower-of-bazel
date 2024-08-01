699ae37a0cb219fa616ddbfefdfc8db3fec5923d
Seto Elkahfi <setoelkahfi@gmail.com>
2024-08-01 16:13:12 +0200

    Profile

--- frontend/splitfire-desktop/app/profile/layout.tsx
+++ frontend/splitfire-desktop/app/profile/layout.tsx
@@ -1,11 +1,4 @@
-import { Metadata } from 'next';
 import React from 'react';
-
-export const metadata: Metadata = {
-  title: "Play",
-  description: "an intelligent exression engine.",
-};
-
 
 export default async function Layout({
   children,
--- frontend/splitfire-desktop/app/profile/page.tsx
+++ frontend/splitfire-desktop/app/profile/page.tsx
@@ -1,28 +1,38 @@
-import { SkeletonCard } from '../_ui/skeleton-card';
+"use client";
 
-export async function generateStaticParams() {
-  const posts = [
-    { slug: "post-1" },
-    { slug: "post-2" },
-    { slug: "post-3" },
-  ];
- 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }))
-}
+import { useContext } from "react";
+import { UserContext } from "../_src/lib/CurrentUserContext";
+import { useLogger } from "../_src/lib/logger";
+import { SkeletonCard } from "../_ui/skeleton-card";
+import { usernameOrId } from "../_src/models/user";
 
 export default function Page() {
+  const log = useLogger("Profile/Page");
+  const currentUser = useContext(UserContext);
+  log.debug("currentUser");
+
+  const user = currentUser?.user?.user;
+  // Sanity check
+  if (!user) {
+    log.error("No user context");
+    return <div>No user context</div>;
+  }
+
   return (
     <div className="prose prose-sm prose-invert max-w-none">
-      <div className="max-w-none">
-        Your profile
-      </div>
-      <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-        <h2 className="text-2xl font-bold">Your plays</h2>
-        {Array.from({ length: 6 }).map((_, i) => (
-          <SkeletonCard key={i} />
-        ))}
+      <div className="grid grid-rows-2 grid-flow-row auto-rows-max">
+        <div className="max-h-1">
+          <h2>@{usernameOrId(user)}</h2>
+          <p>{user.name}</p>
+        </div>
+        <div className="max-w-none">
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+            <h2 className="text-2xl font-bold">Your plays</h2>
+            {Array.from({ length: 6 }).map((_, i) => (
+              <SkeletonCard key={i} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
