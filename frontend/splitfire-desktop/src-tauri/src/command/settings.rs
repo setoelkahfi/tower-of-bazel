965aa59e0caadb5caf63a512d1c17d70a23a4d59
Seto Elkahfi <setoelkahfi@gmail.com>
2024-08-02 17:21:11 +0200

    fix debug setting

--- frontend/splitfire-desktop/src-tauri/src/command/settings.rs
+++ frontend/splitfire-desktop/src-tauri/src/command/settings.rs
@@ -1,6 +1,12 @@
-use log::{debug, warn};
-use crate::{command::constants::API_HOST_DEVELOPMENT, models::{player::TauriResponse, settings::{Environment, SetEnvironmentResponse}}};
 use super::constants::{API_HOST_PRODUCTION, CURRENT_API_HOST};
+use crate::{
+    command::constants::API_HOST_DEVELOPMENT,
+    models::{
+        player::TauriResponse,
+        settings::{Environment, SetEnvironmentResponse},
+    },
+};
+use log::{debug, warn};
 
 #[tauri::command]
 pub fn set_environment(environment: Environment) -> SetEnvironmentResponse {
@@ -8,16 +14,16 @@ pub fn set_environment(environment: Environment) -> SetEnvironmentResponse {
     let api_host = match environment {
         Environment::Development => &API_HOST_DEVELOPMENT,
         Environment::Production => &API_HOST_PRODUCTION,
-        
     };
     debug!("Will set API host to: {}", api_host);
     unsafe {
+        let _ = CURRENT_API_HOST.take();
         match CURRENT_API_HOST.set(api_host) {
             Ok(_) => debug!("Did set CURRENT_API_HOST."),
             Err(e) => warn!("Failed to set CURRENT_API_HOST: {:?}", e),
         };
     };
-    
+
     SetEnvironmentResponse {
         status: TauriResponse::Success,
         message: "CURRENT_API_HOST set done.".to_string(),
@@ -32,21 +38,21 @@ pub fn get_environment() -> Option<Environment> {
         _ => {
             warn!("Unknown api host!!!");
             return None;
-        },
+        }
     };
     debug!("Current environment: {}", env);
     Some(env)
 }
 
 pub fn api_host() -> &'static str {
-  match unsafe { CURRENT_API_HOST.get() } {
-      Some(host) => {
-          debug!("CURRENT_API_HOST: {}", host);
-          host
-      },
-      None => {
-          warn!("API host not set. Defaulting to production.");
-          API_HOST_PRODUCTION
-      },
-  }
+    match unsafe { CURRENT_API_HOST.get() } {
+        Some(host) => {
+            debug!("CURRENT_API_HOST: {}", host);
+            host
+        }
+        None => {
+            warn!("API host not set. Defaulting to production.");
+            API_HOST_PRODUCTION
+        }
+    }
 }
